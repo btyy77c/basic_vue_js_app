4895abff6b853a832709da1ca351f474a272d765
Emily Ring <emily_ring@ymail.com>
2019-09-11 13:20:16 -0400

    Started Navbar styles

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
   </div>
 </template>
 
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
 <script>
 // @ is an alias to /src
 import Nav from "@/components/Nav.vue";
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav id="nav">
+    <div class="logo">
+      <span>Hurricane Dorian</span>
+      <span>Relief</span>
+    </div>
+
+    <div class="links">
+      <p>Donate</p>
+      <p>FAQ</p>
+      <p>Contact</p>
+      <router-link to="/">Donate</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
 </template>
+
+<style scoped lang="scss">
+nav {
+  background-color: #00788c;
+  color: #fff;
+  display: flex;
+}
+
+.links {
+  align-items: center;
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+}
+
+.logo {
+  align-items: center;
+  background-color: #ffc827;
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 15px;
+}
+</style>

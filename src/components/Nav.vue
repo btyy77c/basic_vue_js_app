a2e002adf923960b42d872abfda90746d1a2e592
Emily Ring <emily_ring@ymail.com>
2019-09-12 18:21:43 -0400

    Nav style updates

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,20 +2,29 @@
   <nav id="nav">
     <div class="logo">
       <span>Hurricane Dorian</span>
+      <i class="material-icons icon-image-preview">favorite_border</i>
       <span>Relief</span>
     </div>
 
     <i
       @click="displayLinks = !displayLinks"
-      class="material-icons icon-image-preview"
+      class="material-icons icon-image-preview menu"
     >
       menu
     </i>
 
     <div :class="[displayLinks ? 'display' : 'hidden', 'links']">
-      <router-link to="/">Donate</router-link>
-      <router-link to="/faq">FAQ</router-link>
-      <router-link to="/contact">Contact</router-link>
+      <router-link to="/" @click.native="displayLinks = false">
+        Donate
+      </router-link>
+
+      <router-link to="/faq" @click.native="displayLinks = false">
+        FAQ
+      </router-link>
+
+      <router-link to="/contact" @click.native="displayLinks = false">
+        Contact
+      </router-link>
     </div>
   </nav>
 </template>
@@ -23,22 +32,6 @@
 <style scoped lang="scss">
 @import "../assets/nav";
 @import "../assets/nav_desktop";
-
-/*
-
-a {
-  align-items: center;
-  display: flex;
-  height: 100%;
-  padding: 0 10px;
-}
-
-.links {
-  align-items: center;
-  flex-grow: 1;
-  justify-content: flex-end;
-}
-*/
 </style>
 
 <script>

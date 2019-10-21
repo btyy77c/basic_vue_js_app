b3a394be88cb2989150d9e9052268d8f10117765
Emily <emily_ring@ymail.com>
2019-10-21 15:56:56 -0400

    Linter fixes

--- tests/unit/paymentForm.spec.js
+++ tests/unit/paymentForm.spec.js
@@ -33,7 +33,7 @@ describe("PaymentForm.vue", () => {
     // Amount valid
     wrapper.setData({ amount: 5, cardErrors: null });
     wrapper.vm.fakePayment();
-    expect(wrapper.emitted()).toEqual({ "fake-token": [ [ null, '5' ] ] })
+    expect(wrapper.emitted()).toEqual({ "fake-token": [[null, "5"]] });
     expect(wrapper.vm.$data.cardErrors).toEqual(null);
   });
 
@@ -43,8 +43,9 @@ describe("PaymentForm.vue", () => {
     wrapper.setData({ amount: -1 });
     expect(wrapper.vm.$data.amount).toEqual("1");
 
-
-    wrapper.setData({ amount: "1,.6jbyqe78697726`976t7`2:(&G%@#!bvyeqf8980`=+)" });
+    wrapper.setData({
+      amount: "1,.6jbyqe78697726`976t7`2:(&G%@#!bvyeqf8980`=+)"
+    });
     expect(wrapper.vm.$data.amount).toEqual("1678697726976728980");
   });
 });

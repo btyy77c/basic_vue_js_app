88997d5ce93a0e79dc9d99266643d35dd9ebc21e
Emily <emily_ring@ymail.com>
2019-10-21 15:38:20 -0400

    Added valid form test

--- tests/unit/paymentForm.spec.js
+++ tests/unit/paymentForm.spec.js
@@ -3,16 +3,32 @@ import PaymentForm from "@/components/PaymentForm.vue";
 
 describe("PaymentForm.vue", () => {
   test("is a Vue instance", () => {
-    const wrapper = mount(PaymentForm, {
-      stubs: ["router-link"],
-      stripe: null
-    });
+    const wrapper = mount(PaymentForm, { stubs: ["router-link"] });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
-  /*
-  test("filterAmount removes non numbers from amount", () => {});
+  test("validAmount checks for positive integers", () => {
+    // Positive integer
+    const wrapper = mount(PaymentForm, { stubs: ["router-link"] });
+    wrapper.setData({ amount: 10 });
+    expect(wrapper.vm.validAmount).toBe(true);
 
-  test("validAmount returns true or false", () => {});
-  */
+    // Negative integer
+    wrapper.setData({ amount: 0 });
+    expect(wrapper.vm.validAmount).toBe(false);
+
+    // Not a number
+    wrapper.setData({ amount: "not a number" });
+    expect(wrapper.vm.validAmount).toBe(false);
+  });
+
+  test("fakePayment submits card data to parent component", () => {
+    // Amount not valid
+
+    // Amount valid
+  });
+
+  test("amount watcher removes non numbers from amount", () => {
+
+  });
 });

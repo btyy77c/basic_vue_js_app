4d2067e3f4eb20f42cd102801c252b5ff42bf019
Emily <emily_ring@ymail.com>
2019-10-24 15:20:32 -0400

    Clean up Vuex testing data

--- tests/unit/components/paymentPassed.spec.js
+++ tests/unit/components/paymentPassed.spec.js
@@ -1,30 +1,21 @@
-import { mount, createLocalVue } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Vuex from "vuex";
 import PaymentPassed from "@/components/PaymentPassed.vue";
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
+import Store from "@/store.js";
 
 describe("PaymentPassed.vue", () => {
-  let actions
   let store
 
   beforeEach(() => {
-    actions = {
-      actionClick: jest.fn(),
-      actionInput: jest.fn()
-    }
-    store = new Vuex.Store({
-      actions
-    })
+    store = Store
   });
 
   test("is a Vue instance", () => {
-    const wrapper = mount(PaymentPassed, { store, localVue });
+    const wrapper = mount(PaymentPassed, { store });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
   test("updatePaymentStatus changes store to accepting new donations", () => {
-    const wrapper = mount(PaymentPassed, { store, localVue });
+    const wrapper = mount(PaymentPassed, { store });
   });
 });

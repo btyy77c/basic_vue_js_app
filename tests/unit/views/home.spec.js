fb14ed607cc538a0eca18cf02e3e6d74abb5c7ff
Emily <emily_ring@ymail.com>
2019-10-24 16:39:10 -0400

    Updated home component testing

--- tests/unit/views/home.spec.js
+++ tests/unit/views/home.spec.js
@@ -2,27 +2,34 @@ import { mount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 import Store from "@/store.js";
 
-describe("PaymentPassed.vue", () => {
-  const Stripe = () => ({
+const Stripe = jest.fn(() => {
+  return {
     elements: jest.fn(),
-    createToken: jest.fn(() => Promise.resolve())
-  });
 
-  let store;
+    createToken: jest.fn(card => {
+      return new Promise((resolve, reject) => {
+        resolve({})
+        reject({})
+      })
+    })
+  }
+});
 
+let store;
+let wrapper;
+
+describe("PaymentPassed.vue", () => {
   beforeEach(() => {
     store = Store;
     window.Stripe = Stripe;
+    wrapper = mount(Home, { store });
   });
 
   test("is a Vue instance", () => {
-    const wrapper = mount(Home, { store });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
   test("completedPayment", () => {
-    const wrapper = mount(Home, { store });
-
     // returns false when donationStatus is not completed
     wrapper.vm.$store.commit("changeDonationStatus", "testing");
     expect(wrapper.vm.completedPayment).toEqual(false);
@@ -32,8 +39,26 @@ describe("PaymentPassed.vue", () => {
     expect(wrapper.vm.completedPayment).toEqual(true);
   });
 
-  test("fakeStripeToken", () => {
+  test("fakeStripeToken", async () => {
+    // setup
+    wrapper.vm.$store.commit("changeDonationStatus", "testing");
+    expect(wrapper.vm.$store.state.donationStatus).toEqual("testing");
+
+    wrapper.vm.$store.commit("changeLastDonatedAmount", 2)
+    expect(wrapper.vm.$store.state.lastDonatedAmount).toEqual(2);
+
     // does nothing if processingCard is true
+    wrapper.setData({ processingCard: true });
+
+    await wrapper.vm.fakeStripeToken({}, 10);
+    expect(wrapper.vm.$store.state.donationStatus).toEqual("testing");
+    expect(wrapper.vm.$store.state.lastDonatedAmount).toEqual(2);
+
     // updates store values if processingCard is false
+    wrapper.setData({ processingCard: false });
+
+    await wrapper.vm.fakeStripeToken({}, 10)
+    expect(wrapper.vm.$store.state.donationStatus).toEqual("completed");
+    expect(wrapper.vm.$store.state.lastDonatedAmount).toEqual(10);
   });
 });
